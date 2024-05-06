fed517c8ac372d3eaabce53b96e28ba1271dd4b3
NigerianHacker <58698815+NigerianHacker@users.noreply.github.com>
2024-05-06 11:38:36 +0200

    Update bigram.py

--- BiGram Model/bigram.py
+++ BiGram Model/bigram.py
@@ -5,6 +5,9 @@ import sys
 class LanguageModel():
     def __init__(self):
         self.bigrams = {}
+        self.token_sequence = []
+        self.vocab_size = 0
+        self.vocab = set()
 
     def pre_process(self, corpus):
         lines = list(corpus.splitlines())
@@ -12,19 +15,22 @@ class LanguageModel():
         for index in range(len(lines)):
             if len(lines[index]) == 0:
                 continue
-            lines[index] = re.sub(r'[^\w\s]', '', lines[index])
-            # lines[index] = '<s> ' + lines[index]
+            lines[index] = re.sub(r'[^\w\s]', '', lines[index]) # reove non-words like punctiuation and digits 
+            lines[index] = ' <s>' + lines[index]
             lines[index] = re.sub(r'[0-9]', '', lines[index])
             lines[index] = lines[index].lower()
-            # lines[index] += ' </s> '
+            lines[index] += ' </s> '
     
         token_sequence = ''.join(lines).split()
+        self.token_sequence = token_sequence
         return token_sequence
     
     
     def train(self, corpus):
         token_sequence = self.pre_process(corpus)
         vocab = set(token_sequence)
+        self.vocab_size = len(vocab)
+        self.vocab = vocab
         bigrams = {}
         
         for i in range(len(token_sequence)-1):
@@ -32,49 +38,60 @@ class LanguageModel():
             next_token = token_sequence[i + 1]
       
             if curent_token not in bigrams:
-                bigrams[curent_token] = {'internal_count' : 0} # init the current word to 0
-                
+                # init the current word to 1 beacsue we use add-one smoothing 
+                bigrams[curent_token] = {'internal_count' : 1}
             bigrams[curent_token]['internal_count'] += 1 # now increment because we have found a token (an instance) of a word 
             
             if next_token not in bigrams[curent_token]: # same thing for the next word (token of a word)
-                bigrams[curent_token][next_token] = 0 
-                
+                bigrams[curent_token][next_token] = 1
             bigrams[curent_token][next_token] += 1
         
         self.bigrams = bigrams
-        # print(bigrams)
     
     def bigram_probaility(self, bigram):
         # print("Current Bigram:", bigram)
         # print("Current Bigram[0]:", bigram[0])
+                                                                              #   bigrams = {'the': {'internal_count': 2,
+                                                                              #              'project': 1}
         # print("Current Bigram[1]:", bigram[1])
         # print("Self Bigrams:", self.bigrams)
+                                                                              #   bigram = ['the', 'project']
         # bigrams = self.count_bigrams(corpus)
+
+        # ### linear interpolation #################
+        # try:
+        #     a = 1 / 2
+        #     prob_inter = a * (self.bigrams[bigram[0]]['internal_count'] / self.vocab_size) + \
+        #     (1 - a) * (self.bigrams[bigram[0]][bigram[1]] / self.bigrams[bigram[0]]['internal_count'])
+        #     return prob_inter
+        # except KeyError:
+        #     return 1 / self.vocab_size
+        # ########################################## ^ (174.5604163134672 195.326091981654) ^
+        
         try:
             # P(w2|w1) = C(w1,w2) / C(w1)
-            probability = self.bigrams[bigram[0]][bigram[1]] / (self.bigrams[bigram[0]]['internal_count']) 
-            return probability # bigrams[bigram[0]][bigram[1]]/(bigrams[bigram[0]]['internal_count'])
-        except KeyError as e:
-             return 0 # (f'The word {e} is not part of our corpus, or this bigram doesn\'t co-occur')
+            probability = (self.bigrams[bigram[0]][bigram[1]]) / (self.bigrams[bigram[0]]['internal_count'])
+            return probability
+        except KeyError:
+            try:
+                unigram_probability = (self.bigrams[bigram[0]]['internal_count']) / self.vocab_size
+                return unigram_probability
+            except KeyError:
+                return 1 / self.vocab_size
+        ############################################ ^ (140.59207466850373 147.06623612019703) ^
     
-    
-    def perplexity(self, corpus): # PP(W) = P(w1,w2,...,wn) ** -1/N 
+    def perplexity(self, corpus): # PP(W) = P(w1,w2,...,wn) ** -1 / N 
         token_sequence = self.pre_process(corpus) # W
         num_bigrams = len(token_sequence) # N
         perplexity = 1 # product starts from 1 
+
         for i in range(num_bigrams - 1):
             bigram = [token_sequence[i],token_sequence[i + 1]] # wn
             probability = self.bigram_probaility(bigram) # P(w)
-
-            # here we should add smooting because somethimes we end up with zero probability 
-            if probability == 0:
-                probability = sys.float_info.min
-                
             perplexity *= probability ** (- 1 / num_bigrams) # PP(W) 
-            
-            # print(f'{bigram} --> {probaility}')
+            #print(f'{bigram} --> {probability}')
         return perplexity
-
+    
     ### Additional Experiments (5 additional points - Optional)
     def generate_sentence(self):
         sentence = ['<s>']
@@ -87,7 +104,7 @@ class LanguageModel():
             next_tokens = list(self.bigrams[current_token].keys())
             next_tokens.pop(0) # remove our internal_counter 
             probabilities = [self.bigram_probaility((current_token, next_token)) for next_token in next_tokens]
-
+            
             next_token = random.choices(next_tokens, weights=probabilities)[0]
             sentence.append(next_token)
             current_token = next_token
@@ -96,14 +113,18 @@ class LanguageModel():
         generated_sentence = ' '.join(sentence)
         return generated_sentence
 
+corpus_jungle = (open("jungle_book.txt")).read()
+
+corpus_wiki_train = (open('Datasets_and_resources/wiki.train.raw')).read()
+corpus_wiki_valid = (open('Datasets_and_resources/wiki.valid.raw')).read()
+corpus_wiki_test = (open('Datasets_and_resources/wiki.test.raw')).read()
+
+# models_corpus = open('models.txt').read()
+# под_игото = (open('Ivan_Vazov_-_Pod_igoto_-_1773-b.txt')).read()
 
 model = LanguageModel()
+model.train(corpus_wiki_train)
 
-corpus_jungle = (open("jungle_book.txt")).read()
-corpus_wiki_test = (open('wiki.test.raw')).read()
-corpus_wiki_train = (open('wiki.train.raw')).read()
+print(model.perplexity(corpus_wiki_valid), model.perplexity(corpus_wiki_test))
 
-model.train(models_corpus)
-model.perplexity(models_corpus)
-
-model.generate_sentence()
+# model.generate_sentence()
